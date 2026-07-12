a3a487d8f71ffafbc6421621a77bc61ba604b569
ggm77 <shmshm86@naver.com>
2026-07-12 14:36:08 +0900

    first commit

---
+++ src/main/java/com/seohamin/pomoland/PomolandApplication.java
@@ -0,0 +1,13 @@
+package com.seohamin.pomoland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomolandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomolandApplication.class, args);
+	}
+
+}
